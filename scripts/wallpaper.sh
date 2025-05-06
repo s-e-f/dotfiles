4cafe198ebcbc8b78a1a4e06fd1dddcd06fdae54
Sef <39380372+s-e-f@users.noreply.github.com>
2025-05-06 21:26:38 +0200

    wallpaper randomizer

---
+++ scripts/wallpaper.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+WALLPAPER_DIR="$HOME/wallpapers/"
+CURRENT=$(hyprctl hyprpaper listloaded)
+
+WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+
+hyprctl hyprpaper reload ,"$WALLPAPER"
